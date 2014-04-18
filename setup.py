d0bcaf870016b0edfc8b4564f2f4d1395b2f884c
Davide Moro <davide.moro@redomino.com>
2014-04-18 17:09:34 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
 
-version = '0.0'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.1'
+
+long_description = (
+      read('README.txt')
+      + '\n' +
+      'Change history\n'
+      '**************\n'
+      + '\n' +
+      read('CHANGES.txt')
+      )
 
 setup(name='lfs_trackactivity',
       version=version,
       description="Django LFS ecommerce activity tracker",
-      long_description="""\
-Activity tracker powered by adform.com""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      long_description=long_description,
+      classifiers=["Framework :: Django",
+                   "Programming Language :: Python",], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='Python Django ecommerce',
       author='Davide Moro',
       author_email='davide.moro@redomino.com',
-      url='http://redomino.com',
+      url='https://github.com/redomino/lfs_trackactivity',
       license='GPL',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
